bfa338029ed289b583ad2f9a5eebe61569e19a55
Sahil Lamba <sahil.lamba95@gmail.com>
2015-08-23 14:20:42 +0530

    Finishing Up #3

--- pages/spons/spons.js
+++ pages/spons/spons.js
@@ -1,16 +1,17 @@
 app.controller('SponsController', ['$scope',
   function ($scope) {
-    $scope.whys = ['Team KNOx is an All Terrain Vehicle Project under the SAE India\
-    Chapter of IIT Roorkee which competes in the BAJA SAE India series of competitions.\
-     Sponsorship and donations make a real difference; they help to attract \
-     funding for various student projects which often develop into the \
-     foundations of the future industry.',
+    $scope.whys = ['Team KNOx is an All Terrain Vehicle Project under SAE Chapter\
+    of IIT Roorkee which competes in BAJA SAE India Series of competitions. The\
+    Previous Sponsors are the sole reason our team has been able to take part\
+    in BAJA SAE India since its inception. The team is highly motivated towards\
+    achieving new heights every year. Funding helps secure the confidence of team,\
+    makes them independent and they can be fearless in their innovative designs.',
       'BAJA SAE is not simply an international student competition, but is the \
-     medium for students across the world to get involved with research, \
-     designing, analysis, construction and business experience specifically in \
-     the automotive sector. The sponsorship provided by you would let us expand\
-      our horizons and dive deeper into research and help us solidify what we \
-      imagine, and there we innovate.'
+    medium for students across the world to get involved with research, \
+    designing, analysis, construction and business experience specifically in \
+    the automotive sector. The sponsorship provided by you would let us expand\
+    our horizons and dive deeper into research and help us solidify what we \
+    imagine, and there we innovate.'
     ];
   }
 ]);
