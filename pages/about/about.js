9b4e088e0805560961adb34dd6bc4e322615fe94
Sahil Lamba <sahil.lamba95@gmail.com>
2015-08-23 14:49:24 +0530

    Add description and keywords

--- pages/about/about.js
+++ pages/about/about.js
@@ -6,9 +6,8 @@ app.controller('AboutController', ['$scope',
       	in designing and fabrication of All­-Terrain Vehicles. This team represents IIT Roorkee\
       	at BAJA SAE India, which is the largest annual inter­collegiate competition to be held\
       	across the country where we compete with 300 other colleges. The project is funded by\
-      	the institute to some extent and the rest is the investment of corporate sponsors.\
-      	We work religiously and enjoy every bit of our time in the garage, and winning is\
-      	what we believe in.'
+        corporate sponsors. We work religiously and enjoy every bit of our time in the garage,\
+         and winning is what we believe in.'
     };
 
     $scope.aim = {
